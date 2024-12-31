ae168d35a1ac88137cc8c45d63415e3cd2f2c3e9
Timothy Roy <timothyroy94@protonmail.com>
2024-12-31 10:15:04 +0100

    env variable addded

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
+const apiBaseUrl = process.env.REACT_APP_API_BASE_URL
 const api = axios.create({
-    baseURL: "https://bookstore-api-n50c.onrender.com/",
+    baseURL: apiBaseUrl,
 });
 
 export default api;
